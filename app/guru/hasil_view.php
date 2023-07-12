578dd236367d31b171772523aa1cb3f980be1e66
Rianfirmansyah071020 <rian071020@gmail.com>
2023-07-12 10:56:44 +0700

    selesai

--- app/guru/hasil_view.php
+++ app/guru/hasil_view.php
@@ -1,11 +1,4 @@
 <?php
-session_start();
-
-    if($_SESSION['login'] !== true) {
-
-    header('location:../logout/index.php');
-    exit;
-    }
 
 require "../controller/controller.php";
 
@@ -25,18 +18,9 @@ require "../controller/controller.php";
             <ul class="navbar-nav ml-auto">
                 <div class="topbar-divider d-none d-sm-block"></div>
                 <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="../../index.php">
                         <span class="ml-2 d-none d-lg-inline text-white small">kembali</span>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                        aria-labelledby="userDropdown">
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="../../index.php" data-toggle="modal" data-target="#logoutModal">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                            kembali
-                        </a>
-                    </div>
                 </li>
             </ul>
         </nav>
